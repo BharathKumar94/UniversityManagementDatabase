c75e7bb139c28fa5736d736450ea9bf6090a4743
BharathKumar94 <bhachikumar@gmail.com>
2017-04-30 12:54:31 -0700

    Add files via upload

---
+++ DBConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollegeSystem
+{
+    class DBConnection
+    {
+        public static string connectionstring = "Data Source = LAPTOP-AD7ALG4C\\MSSQLSERVER01;Initial Catalog= CollegesystemDB1;Integrated Security=True";
+
+    }
+}
